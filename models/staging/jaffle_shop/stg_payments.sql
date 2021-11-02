901de0664ef871a957aa1cf3dd72b2525eb19686
serhii.moria@globallogic.com <serhii.moria@globallogic.com>
2021-11-02 18:27:44 +0000

    incremental

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -7,7 +7,7 @@ with payments as (
         status,
 
         -- amount is stored in cents, convert it to dollars
-        amount / 100 as amount,
+        {{ cents_to_dollars('amount', 4) }} as amount,
         created as created_at
 
     from raw.stripe.payment
